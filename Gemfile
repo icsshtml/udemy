f8b08d6fea901d6c675b0a224b11344ccd9dbb88
Jonathan Li <icsshtml@gmail.com>
2016-04-09 07:25:26 +0000

    lesson 23

--- Gemfile
+++ Gemfile
@@ -51,4 +51,8 @@ gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 gem 'devise', '>= 3.2.4'
 gem 'simple_form'
+gem 'carrierwave'
+gem 'figaro'
+gem 'fog'
+gem 'mini_magick', '~> 4.3.1'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408223621) do
+ActiveRecord::Schema.define(version: 20160409041626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160408223621) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "lessons", force: :cascade do |t|
